cfd9802fea921cf3ffd947c7ff30c3e6203fff65
chriswillphoto <christophernekich@gmail.com>
2018-02-11 14:12:20 +1100

    hearts now beat to click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 const heartbeat = document.querySelector("audio");
-
+const heart1 = document.querySelector(".heart1");
+const heart2 = document.querySelector(".heart2");
 document.onclick = function() {
   heartbeat.play();
+  heart1.classList = "heart1 enlarge1";
+  heart2.classList = "heart2 enlarge2";
+  setTimeout(function() {
+    heart1.classList = "heart1";
+    heart2.classList = "heart2";
+  }, 600);
 };
