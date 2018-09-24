3063a6acc68ebad01d87c3bb2598c31616a475fa
chriswillphoto <christophernekich@gmail.com>
2018-09-24 21:56:49 +1000

    bpm counter, audio reset on click

--- js/main.js
+++ js/main.js
@@ -1,12 +1,73 @@
 const heartbeat = document.querySelector("audio");
 const heart1 = document.querySelector(".heart1");
 const heart2 = document.querySelector(".heart2");
-document.onclick = function() {
-  heartbeat.play();
-  heart1.classList = "heart1 enlarge1";
-  heart2.classList = "heart2 enlarge2";
-  setTimeout(function() {
-    heart1.classList = "heart1";
-    heart2.classList = "heart2";
-  }, 600);
-};
+
+let current = Date.now();
+let newBeat;
+
+function reset_animation() {
+  var el = document.getElementById("enlarge1");
+  el.style.animation = "none";
+  el.offsetHeight; /* trigger reflow */
+  el.style.animation = null;
+}
+
+// $("body").on("click", function() {
+//   if (!heartbeat.paused || heartbeat.currentTime){
+//     heartbeat.load();
+//   }
+//   $(".animated").removeClass("animated");
+//   heart1.style.transition = "none"
+//   heartbeat.play();
+//   $(this).addClass("animate");
+// });
+
+// $("body").on("webkitAnimationEnd oanimationend msAnimationEnd animationend", function() {
+//   $(this).removeClass("animate");
+// });
+
+$(function() {
+  // Shorthand for $( document ).ready()
+  $("body").click(function(e) {
+    e.preventDefault();
+
+    if ($(".animate").length == 0) {
+      current = newBeat || current;
+      newBeat = Date.now();
+
+      var seconds = (newBeat - current) / 1000;
+
+      var test = 60 / seconds;
+
+      $(".bpm-counter").text(Math.floor(test) + " BPM");
+
+      if (!heartbeat.paused || heartbeat.currentTime) {
+        heartbeat.load();
+      }
+
+      heartbeat.playbackRate = 1.5;
+      heartbeat.play();
+    }
+
+    $(this)
+      .addClass("animate")
+      .one(
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+        function() {
+          $(this).removeClass("animate");
+        }
+      );
+  });
+});
+
+// function beatCounter() {
+//   window.requestAnimationFrame(beatCounter);
+
+//   const oldText = $(".bpm-counter").text().trim();
+//   const getNum = oldText.split(" ")[0];
+//   const getNumToInt = parseInt(getNum);
+
+//   $(".bpm-counter").text(`${getNumToInt + 1} BPM`);
+// }
+
+// window.requestAnimationFrame(beatCounter);
